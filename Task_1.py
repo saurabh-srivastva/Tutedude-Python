f0bda7a85ee8c43166f89d55d2480ff49665f507
saurabh-srivastva <Saurabh.dseu@gmail.com>
2025-07-01 00:13:37 +0530

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,10 @@
+num_1=int(input("Enter first number: "))
+num_2=int(input("Enter second number: "))
+addition=num_1+num_2        
+subtraction=num_1-num_2
+multiplication=num_1*num_2 
+division=num_1/num_2
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
---
+++ Task_2.py
@@ -0,0 +1,3 @@
+name=input("Enter your name: ")
+surname=input("Enter your surname: ")
+print("Hello", name, surname, "!","welcome to the Python programming world!")
